c5de85aa68cb5b72ab33fcc8c65dcfc694305289
khw <khw0726@kaist.ac.kr>
2017-01-07 22:47:28 +0900

    Simple statistics done

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
         <!--<auth :users="users"></auth>-->
         <Diary :my-article="targetArticle" :remove-article="removeArticle"></Diary>
         <Edit :diary="diary" :onSubmit="addEntry"></Edit>
+        <MyChart :chart-data="chartData" :options="null"></MyChart>
       </div>
 
       <div class="col-md-3 col-md-offset-1">
@@ -23,9 +24,13 @@ import Diary from './components/Diary'
 import Sidebar from './components/Sidebar'
 import Edit from './components/Edit'
 import Auth from './components/Auth'
+import MyChart from './components/MyChart'
 import db from './db'
 
 const diaryRef = db.ref('diary')
+// const sentiments = diaryRef.on('value', function (snapshot) {
+//   snapshot.child('sentiment').val()
+// })
 // const usersRef = auth
 export default {
   name: 'app',
@@ -33,10 +38,13 @@ export default {
     Diary,
     Sidebar,
     Edit,
-    Auth
+    Auth,
+    MyChart
   },
   firebase: {
-    diary: diaryRef
+    diary: diaryRef.orderByChild('date')
+    // dates: diaryRef.on('value'),
+    // sentiments: diaryRef.on('value')
   },
   data: function () {
     return ({
@@ -46,8 +54,30 @@ export default {
         keyPhrases: [],
         sentiment: -1,
         date: ''
+      },
+      dates: this.getDates(),
+      sentiments: this.getSentiments(),
+      chartData: {
+        labels: this.dates,
+        datasets: [{
+          label: 'Sentiments',
+          data: this.sentiments
+        }]
       }
     })
+  },
+  watch: {
+    diary: function () {
+      this.dates = this.getDates()
+      this.sentiments = this.getSentiments()
+      this.chartData = {
+        labels: this.dates,
+        datasets: [{
+          label: 'Sentiments',
+          data: this.sentiments
+        }]
+      }
+    }
   },
   methods: {
     addEntry (newEntry) {
@@ -67,6 +97,17 @@ export default {
       this.targetArticle.contents = ''
       this.targetArticle.response = ''
       this.targetArticle.date = ''
+    },
+    getDates () {
+      return this.diary.map(function (diary) {
+        let a = new Date(diary.date)
+        return a.date
+      })
+    },
+    getSentiments () {
+      return this.diary.map(function (diary) {
+        return diary.sentiment
+      })
     }
     // tryLogin (user) {
     //   usersRef.signInWithEmailAndPassword(user.email, user.password).catch(function (err) {
---
+++ src/components/MyChart.vue
@@ -0,0 +1,31 @@
+<script>
+  import { Bar, mixins } from 'vue-chartjs'
+  export default Bar.extend({
+    name: 'mychart',
+    mixins: [mixins.reactiveProp],
+    props: ['chartData', 'options'],
+    mounted () {
+      console.log(this.chartData)
+      // let dates = this.diary.map(function (diary) {
+      //   console.log('date' + diary.date)
+      //   return diary.date
+      // })
+      // let sentiments = this.diary.map(function (diary) {
+      //   console.log('sentiment' + diary.sentiment)
+      //   return diary.sentiment
+      // })
+      // this.renderChart({
+      //   labels: this.dates,
+      //   //  ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      //   datasets: [
+      //     {
+      //       label: 'Sentimental Analysis Result',
+      //       backgroundColor: '#f87979',
+      //       data: this.sentiments
+      //     }
+      //   ]
+      // })
+      this.renderChart(this.chartData, this.options)
+    }
+  })
+  </script>
