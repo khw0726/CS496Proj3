3dd9d6101d61468935e6d6363782aabd1117bf3e
junsooo <junsoolee@kaist.ac.kr>
2017-01-09 00:48:56 +0900

    Twitter Upload Completed

--- src/App.vue
+++ src/App.vue
@@ -19,13 +19,99 @@
   </div>
 </template>
 <script>
+/*eslint-disable */
 import Diary from './components/Diary'
 import Sidebar from './components/Sidebar'
 import Edit from './components/Edit'
 import Auth from './components/Auth'
-import db from './db'
+import fb from './db'
+import firebase from 'firebase'
+let db = fb.database();
+
+//Create user with form - Use Callback
+var email = 'junsoo@naver.com';
+var password = 'qwerasdf';
+
+/*
+fb.auth().createUserWithEmailAndPassword(email, password)
+.catch(function(error){
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  if (errorCode == 'auth/weak-password') {
+    alert('The password is too weak.');
+  } else {
+    alert(errorMessage);
+  }
+  console.log(error);
+});
+*/
+
+//Google login with firebase - Use Callback
+
+var provider = new firebase.auth.GoogleAuthProvider();
+provider = new firebase.auth.TwitterAuthProvider();
+//provider.addScope('https://www.googleapis.com/auth/plus.login');
+console.log(provider);
+//provider.addScope('publish_actions')
+//provider.addScope('')
+
+var token;
+var credential;
+fb.auth().signInWithPopup(provider).then(function(result){
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  token = result.credential.accessToken;
+  credential = result.credential;
+  console.log(credential);
+  // The signed-in user info.
+  var user = result.user;
+  //console.log(user);
+  //console.log('Hello,' +user.displayName);
+  //alert('Hello,' +user.displayName);
+  // ...
+  //user = user.delete();
+  //console.log(user);
+  var diary='1';
+  var response='1';
+  var user_token=credential.accessToken;
+  var user_secret=credential.secret;
+  var xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://ec2-52-79-161-158.ap-northeast-2.compute.amazonaws.com:3000/?diary=diary:%20'+diary+'%0A&response=junsoo:%20'+response+'&user_token='+user_token+'&user_secret='+user_secret, true)
+
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          console.log(xhr.response)
+        }
+      }
+     
+      xhr.send()
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+});
+
+
+//POST https://api.twitter.com/1.1/statuses/update.json?status=1234
+
+//Auth
+
+/*
+fb.auth().signOut().then(function() {
+  // Sign-out successful.
+  alert('Sign out completed');
+}, function(error) {
+  // An error happened.
+  alert('Error happened');
+});
+*/
+
 
 const diaryRef = db.ref('diary')
+
 // const usersRef = auth
 export default {
   name: 'app',
--- src/db.js
+++ src/db.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase'
-
+/*eslint-disable */
 var config = {
-  apiKey: 'AIzaSyAHW_T08AWFfQdkV8fFXXEPRizdFWXpwnQ',
-  authDomain: 'test001-9286a.firebaseapp.com',
-  databaseURL: 'https://test001-9286a.firebaseio.com',
-  storageBucket: 'test001-9286a.appspot.com',
-  messagingSenderId: '650810461631'
-}
+    apiKey: "AIzaSyCGUiEq7RzyYn1wne4rwJZtEZn1m6QtJV0",
+    authDomain: "test-web-91d3a.firebaseapp.com",
+    databaseURL: "https://test-web-91d3a.firebaseio.com",
+    storageBucket: "test-web-91d3a.appspot.com",
+    messagingSenderId: "464548710498"
+  };
 
 let fb = firebase.initializeApp(config)
-let db = fb.database()
+//let db = fb.database()
 // let auth = fb.auth()
 
-export default db
+export default fb
